fffb85250f62fc521536d71a7ea37a350ba0c689
Cam Spiers <camspiers@gmail.com>
2011-06-30 01:12:12 +1200

    Interface for arguing a fortiori

--- js/script.js
+++ js/script.js
@@ -2,17 +2,38 @@
 
 	$(function () {
 
+		var hypothesisTemplate = _.template($('#hypothesis').html());
+    var template_options = {
+      a_fortiori: false,
+      fields: [
+        {
+          name: 'hb',
+          label: 'P(H<%= num %>|B)'
+        },
+        {
+          name: 'nhb',
+          label: 'P(~H<%= num %>|B)'
+        },
+        {
+          name: 'ehb',
+          label: 'P(E|H<%= num %>.B)'
+        },
+        {
+          name: 'eb',
+          label: 'P(E|B)'
+        }
+      ]
+    };
+
 		$('#tabs').find('a').click(function (e) {
 			e.preventDefault();
 			$('#panels').find('.panel').removeClass('active').eq($('#tabs').find('a').index($(this))).addClass('active');
 		});
 
-		var hypothesisTemplate = _.template($('#hypothesis').html());
-
 		$('.add_hypothesis').click(function (e) {
 			e.preventDefault();
-      var $hypotheses = $(this).closest('form').find('.hypotheses');
-			$hypotheses.append(hypothesisTemplate({num: $hypotheses.find('.hypothesis').length + 1}));
+      var $hypotheses_container = $(this).closest('form').find('.hypotheses');
+			$hypotheses_container.append(hypothesisTemplate($.extend(template_options, {num: $hypotheses_container.find('.hypothesis').length + 1})));
 		}).click();
     
     $('form').delegate('input[type=range]', 'change', function () {
@@ -24,6 +45,15 @@
       var $this = $(this);
       $this.siblings('input[type=range]').val($(this).val());
     });
+
+		$('.a_fortiori').click(function (e) {
+			e.preventDefault();
+      template_options.a_fortiori = !template_options.a_fortiori;
+      var $hypotheses_container = $(this).closest('form').find('.hypotheses');
+      $hypotheses_container.find('.hypothesis').each(function (index) {
+        $(this).replaceWith(hypothesisTemplate($.extend(template_options, {num: index + 1})));
+      });
+		});
 
 	});
 
