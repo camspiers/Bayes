d61f52f55e95220743ff7b916af482164e9ef13d
metanat <camspiers@gmail.com>
2011-11-02 10:25:53 +0000

    Reworking of javascript

--- js/script.js
+++ js/script.js
@@ -1,84 +1,73 @@
-(function($, bayes, MathJax) {
+(function($, MathJax) {
 
     $(function() {
-
-        var round = function(num, dec) {
+        
+        var bayesapp = {
+            templates: {
+                hypothesis: _.template($('#hypothesis').html())
+            },
+            template_options: {
+                a_fortiori: false,
+                hide_remove: false,
+                fields: [{
+                    name: 'hb',
+                    label: '\\(\\mathrm{\\Pr( H<% if (!single) { %><%= \'_\' + num %><% } %> \\mid b )}\\)',
+                    className: 'hb',
+                    val: 0,
+                    title: 'Probability of the hypothesis given the background evidence',
+                    disabled: false
+                }, {
+                    name: 'nhb',
+                    label: '\\(\\mathrm{\\Pr( \\neg{H}<% if (!single) { %><%= \'_\' + num %><% } %> \\mid b )}\\)',
+                    className: 'nhb',
+                    val: 1,
+                    title: 'Probability of not the hypothesis given the background evidence',
+                    disabled: false
+                }, {
+                    name: 'ehb',
+                    label: '\\(\\mathrm{\\Pr( E \\mid H<% if (!single) { %><%= \'_\' + num %><% } %>.b )}\\)',
+                    className: '',
+                    val: 0,
+                    title: 'Probability of the evidence given the hypothesis and the background evidence',
+                    disabled: false
+                }, {
+                    name: 'enhb',
+                    label: '\\(\\mathrm{\\Pr( E \\mid \\neg{H}<% if (!single) { %><%= \'_\' + num %><% } %>.b )}\\)',
+                    className: '',
+                    val: 0,
+                    title: 'Probability of the evidence given the hypothesis is false and the background evidence',
+                    disabled: false
+                }, {
+                    name: 'eb',
+                    label: '\\(\\mathrm{\\Pr( E \\mid b )}\\)',
+                    className: '',
+                    val: 0,
+                    title: 'Probability of the evidence given the background evidence',
+                    hint: 'Must be greater than 0',
+                    disabled: false
+                }, {
+                    name: 'heb',
+                    label: '\\(\\mathrm{\\Pr( H<% if (!single) { %><%= \'_\' + num %><% } %> \\mid E.b )}\\)',
+                    className: '',
+                    val: 0,
+                    title: 'Probability of the hypothesis given the evidence and given the background evidence',
+                    disabled: true
+                }]
+            },
+            round: function(num, dec) {
                 return Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
-            };
-
-        var hypothesisTemplate = _.template($('#hypothesis').html());
-
-        var template_options = {
-            a_fortiori: false,
-            hide_remove: false,
-            fields: [{
-                name: 'hb',
-                label: '\\(\\mathrm{\\Pr( H<% if (!single) { %><%= \'_\' + num %><% } %> \\mid b )}\\)',
-                className: 'hb',
-                val: 0,
-                title: 'Probability of the hypothesis given the background evidence',
-                disabled: false
-            }, {
-                name: 'nhb',
-                label: '\\(\\mathrm{\\Pr( \\neg{H}<% if (!single) { %><%= \'_\' + num %><% } %> \\mid b )}\\)',
-                className: 'nhb',
-                val: 1,
-                title: 'Probability of not the hypothesis given the background evidence',
-                disabled: false
-            }, {
-                name: 'ehb',
-                label: '\\(\\mathrm{\\Pr( E \\mid H<% if (!single) { %><%= \'_\' + num %><% } %>.b )}\\)',
-                className: '',
-                val: 0,
-                title: 'Probability of the evidence given the hypothesis and the background evidence',
-                disabled: false
-            }, {
-                name: 'enhb',
-                label: '\\(\\mathrm{\\Pr( E \\mid \\neg{H}<% if (!single) { %><%= \'_\' + num %><% } %>.b )}\\)',
-                className: '',
-                val: 0,
-                title: 'Probability of the evidence given the hypothesis is false and the background evidence',
-                disabled: false
-            }, {
-                name: 'eb',
-                label: '\\(\\mathrm{\\Pr( E \\mid b )}\\)',
-                className: '',
-                val: 0,
-                title: 'Probability of the evidence given the background evidence',
-                hint: 'Must be greater than 0',
-                disabled: false
-            }, {
-                name: 'heb',
-                label: '\\(\\mathrm{\\Pr( H<% if (!single) { %><%= \'_\' + num %><% } %> \\mid E.b )}\\)',
-                className: '',
-                val: 0,
-                title: 'Probability of the hypothesis given the evidence and given the background evidence',
-                disabled: true
-            }]
-        };
-
-        $('#tabs').find('a').click(function(e) {
-            e.preventDefault();
-            var $this = $(this);
-            $('#tabs .active').removeClass('active');
-            $this.addClass('active');
-            $('#panels').find('.panel').removeClass('active').eq($('#tabs').find('a').index($this)).addClass('active');
-        });
-
-        var add_hypothesis = function(e, $el) {
+            },
+            add_hypothesis: function(e, $el) {
                 if (e) e.preventDefault();
                 var $hypotheses_container = ($el || $(this)).closest('form').find('.hypotheses');
                 $hypotheses_container.find('.remove').hide();
-                $hypotheses_container.append(hypothesisTemplate($.extend(template_options, {
+                $hypotheses_container.append(bayesapp.templates.hypothesis($.extend(bayesapp.template_options, {
                     num: $hypotheses_container.find('.hypothesis').length + 1,
                     single: $hypotheses_container.hasClass('basic')
                 })));
                 if (MathJax) MathJax.Hub.Queue(["Typeset", MathJax.Hub, $hypotheses_container.get(0)]);
-            };
-
-        add_hypothesis(false, $('.basic'));
-
-        var validate = function($form) {
+            },
+            validate: function($form) {
                 var data = $form.serializeObject(),
                     errors = [],
                     i,
@@ -126,16 +115,26 @@
                         }
                     }                    
                 }
-            };
-
-        var updateCalculation = function(e, $this) {
+            },
+            calculate: function (calc) {
+                if (calc.length == 1) {
+                    if (calc[0].enhb > 0) {
+                        return (parseFloat(calc[0].hb) * parseFloat(calc[0].ehb)) / (parseFloat(calc[0].hb) * parseFloat(calc[0].ehb) + parseFloat(calc[0].nhb) * parseFloat(calc[0].enhb)); 
+                    } else {
+                        return parseFloat(calc[0].eb) != 0 ? (parseFloat(calc[0].hb) * parseFloat(calc[0].ehb)) / parseFloat(calc[0].eb) : 0; 
+                    }
+                } else {
+                    return false;
+                } 
+            },
+            update_calculation: function(e, $this) {
                 if (e) e.preventDefault();
                 $this = !$this ? $(this) : $this;
-                validate($this);
+                bayesapp.validate($this);
                 var data = $this.serializeObject();
                 if (_.isArray(data['eb'])) {
                     for (var i = 0; i < data['eb'].length; i++) {
-                        $this.find('.field-heb .inputs:eq(' + i + ') span').text(round(bayes.calc.posterior([{
+                        $this.find('.field-heb .inputs:eq(' + i + ') span').text(bayesapp.round(bayesapp.calculate([{
                             eb: data['eb'][i],
                             ehb: data['ehb'][i],
                             hb: data['hb'][i],
@@ -145,30 +144,10 @@
                     }
                 }
                 else {
-                    $this.find('.field-heb .inputs span').text(round(bayes.calc.posterior([data]), 2));
+                    $this.find('.field-heb .inputs span').text(bayesapp.round(bayesapp.calculate([data]), 2));
                 }
-            };
-
-        $('.add_hypothesis').click(add_hypothesis).click();
-
-        $('.a_fortiori').click(function(e) {
-            e.preventDefault();
-            template_options.a_fortiori = !template_options.a_fortiori;
-            var $hypotheses_container = $(this).closest('form').find('.hypotheses'),
-                $hypotheses = $hypotheses_container.find('.hypothesis')
-                $hypotheses.each(function(index) {
-                    $(this).replaceWith(hypothesisTemplate($.extend(template_options, {
-                        num: index + 1,
-                        hide_remove: (index + 1) !== $hypotheses.length,
-                        single: $hypotheses_container.hasClass('basic')
-                    })));
-                });
-            if (MathJax) MathJax.Hub.Queue(["Typeset", MathJax.Hub, $hypotheses_container.get(0)]);
-        });
-
-        var $form = $('form');
-
-        var updateJoinedFields = function($this, type) {
+            },
+            update_joined_fields: function($this, type) {
                 var $field = $this.closest('.field'),
                     $fields = $field.siblings('.field'),
                     $inputs = $field.find('.inputs'),
@@ -181,36 +160,71 @@
                 if ($field.hasClass('field-nhb')) {
                     $fields.filter('.field-hb').find('.inputs:eq(' + i + ') input').val(1 - parseFloat($this.val()));
                 }
-                updateCalculation(false, $this.closest('form'));
-            };
-
-        $form.delegate('input[type=range]', 'change', function() {
-            updateJoinedFields($(this), 'number');
-        });
-
-        $form.delegate('input[type=number]', 'change', function() {
-            updateJoinedFields($(this), 'range');
-        });
-
-        $form.delegate('.remove', 'click', function(event) {
-            event.preventDefault();
-            var $this = $(this),
-                $form = $this.closest('form'),
-                $hypothesis = $this.closest('.hypothesis'),
-                $hypotheses = $hypothesis.siblings('.hypothesis');
-            if ($hypotheses.length > 0) {
-                $hypothesis.remove();
-                $form.find('.remove:last').show();
+                bayesapp.update_calculation(false, $this.closest('form'));
+            },
+            init: function () {
+                $('#tabs').find('a').click(function(e) {
+                    e.preventDefault();
+                    var $this = $(this);
+                    $('#tabs .active').removeClass('active');
+                    $this.addClass('active');
+                    $('#panels').find('.panel').removeClass('active').eq($('#tabs').find('a').index($this)).addClass('active');
+                });
+        
+                bayesapp.add_hypothesis(false, $('.basic'));
+        
+                $('.add_hypothesis').click(bayesapp.add_hypothesis).click();
+        
+                $('.a_fortiori').click(function(e) {
+                    e.preventDefault();
+                    bayesapp.template_options.a_fortiori = !bayesapp.template_options.a_fortiori;
+                    var $hypotheses_container = $(this).closest('form').find('.hypotheses'),
+                        $hypotheses = $hypotheses_container.find('.hypothesis')
+                        $hypotheses.each(function(index) {
+                            $(this).replaceWith(bayesapp.templates.hypothesis($.extend(bayesapp.template_options, {
+                                num: index + 1,
+                                hide_remove: (index + 1) !== $hypotheses.length,
+                                single: $hypotheses_container.hasClass('basic')
+                            })));
+                        });
+                    if (MathJax) MathJax.Hub.Queue(["Typeset", MathJax.Hub, $hypotheses_container.get(0)]);
+                });
+        
+                var $form = $('form');
+        
+                $form.delegate('input[type=range]', 'change', function() {
+                    bayesapp.update_joined_fields($(this), 'number');
+                });
+        
+                $form.delegate('input[type=number]', 'change', function() {
+                    bayesapp.update_joined_fields($(this), 'range');
+                });
+        
+                $form.delegate('.remove', 'click', function(event) {
+                    event.preventDefault();
+                    var $this = $(this),
+                        $form = $this.closest('form'),
+                        $hypothesis = $this.closest('.hypothesis'),
+                        $hypotheses = $hypothesis.siblings('.hypothesis');
+                    if ($hypotheses.length > 0) {
+                        $hypothesis.remove();
+                        $form.find('.remove:last').show();
+                    }
+                });
+        
+                $form.delegate('label[title],input[title],button[title]', 'mouseover', function() {
+                    var $this = $(this);
+                    if (!$this.data("tooltip")) {
+                        $this.tooltip({
+                            position: "center right"
+                        }).trigger('mouseover');
+                    }
+                });
             }
-        });
-
-        $form.delegate('label[title],input[title],button[title]', 'mouseover', function() {
-            var $this = $(this);
-            if (!$this.data("tooltip")) {
-                $this.tooltip({position: "center right"}).trigger('mouseover');
-            }
-        });
+        }; //End bayesapp
+        
+        bayesapp.init();
 
     });
 
-}(jQuery, bayes, MathJax));
+}(jQuery, MathJax));
